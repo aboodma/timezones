156db62ac1d68d20bf7ada7e7cd932585efb28c6
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2024-04-07 14:04:13 +0300

    Init

---
+++ src/Facade/TimezonesFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aboodma\EcoTimezones\Facade;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Laracon21\Timezones\Skeleton\SkeletonClass
+ */
+class TimezonesFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'timezones';
+    }
+}
